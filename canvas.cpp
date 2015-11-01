2468078e73e510543bd2aa8065cf64ceb7e57046
AprilWong <aprilwang.zju@gmail.com>
2015-11-01 21:20:43 +0800

    finish drawing shape part, booooomgit add .

--- canvas.cpp
+++ canvas.cpp
@@ -7,9 +7,104 @@ Canvas::Canvas(QWidget *parent) :
     ui(new Ui::Canvas)
 {
     ui->setupUi(this);
+    this->editMethod = 0;
+    this->ui->undoButton->setDisabled("True");
+    this->ui->clearButton->setDisabled("True");
+    this->ui->saveButton->setDisabled("True");
 }
 
 Canvas::~Canvas()
 {
     delete ui;
+}
+
+void Canvas::mousePressEvent(QMouseEvent *event)
+{
+    if(event->button() == Qt::LeftButton) {
+        this->mousePoint = event->pos();
+        this->editPoint();
+        repaint();
+        qDebug()<<this->mousePoint.x()<<this->mousePoint.y();
+    }
+}
+
+void Canvas::paintEvent(QPaintEvent *)
+{
+    paint = new QPainter;
+    paint->begin(this);
+    paint->setPen(QPen(QColor(150, 150, 150), 2));
+    paint->drawRect(25, 80, 750, 280);
+
+    paint->setPen(QPen(QColor(255, 0, 0), 2));
+    for(int i = 0; i < this->begin_shape.point.size(); i ++){
+        paint->drawEllipse(this->begin_shape.point[i], 2, 2);
+    }
+    paint->setPen(QPen(QColor(255, 255, 0), 2));
+    for(int i = 1; i < this->begin_shape.point.size(); i ++) {
+        paint->drawLine(this->begin_shape.point[i-1], this->begin_shape.point[i]);
+    }
+
+    paint->setPen(QPen(QColor(0, 0, 255), 2));
+    for(int i = 0; i < this->end_shape.point.size(); i ++){
+        paint->drawEllipse(this->end_shape.point[i], 2, 2);
+    }
+    paint->setPen(QPen(QColor(0, 255, 255), 2));
+    for(int i = 1; i < this->end_shape.point.size(); i ++) {
+        paint->drawLine(this->end_shape.point[i-1], this->end_shape.point[i]);
+    }
+
+
+}
+
+void Canvas::editPoint()
+{
+    if(this->mousePoint.x() > 25 && this->mousePoint.x() < 775 && this->mousePoint.y() > 80 && this->mousePoint.y() < 360){
+        if(this->editMethod == 1)
+            this->begin_shape.add_point(this->mousePoint);
+        if(this->editMethod == 2)
+            this->end_shape.add_point(this->mousePoint);
+    }
+}
+
+void Canvas::on_beginButton_clicked()
+{
+    this->editMethod = 1;
+    this->ui->endButton->setDisabled("True");
+    this->ui->undoButton->setEnabled("True");
+    this->ui->clearButton->setEnabled("True");
+    this->ui->saveButton->setEnabled("True");
+
+}
+
+void Canvas::on_endButton_clicked()
+{
+    this->editMethod = 2;
+    this->ui->beginButton->setDisabled("True");
+    this->ui->undoButton->setEnabled("True");
+    this->ui->clearButton->setEnabled("True");
+    this->ui->saveButton->setEnabled("True");
+}
+
+void Canvas::on_saveButton_clicked()
+{
+    this->ui->saveButton->setDisabled("True");
+    this->ui->undoButton->setDisabled("True");
+    this->ui->clearButton->setDisabled("True");
+    this->editMethod = 0;
+    this->ui->beginButton->setEnabled("True");
+    this->ui->endButton->setEnabled("True");
+}
+
+void Canvas::on_undoButton_clicked()
+{
+    if(this->editMethod == 1) this->begin_shape.undo();
+    if(this->editMethod == 2) this->end_shape.undo();
+    repaint();
+}
+
+void Canvas::on_clearButton_clicked()
+{
+    if(this->editMethod == 1) this->begin_shape.clear();
+    if(this->editMethod == 2) this->end_shape.clear();
+    repaint();
 }
--- canvas.h
+++ canvas.h
@@ -4,8 +4,9 @@
 #include <QWidget>
 #include <QPainter>
 #include <QDebug>
+#include <QMouseEvent>
 
-#incllude "shape.h"
+#include "shape.h"
 
 namespace Ui {
 class Canvas;
@@ -19,10 +20,34 @@ public:
     explicit Canvas(QWidget *parent = 0);
     ~Canvas();
 
+protected:
+    void mousePressEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
+
+private slots:
+    void on_beginButton_clicked();
+
+    void on_endButton_clicked();
+
+    void on_saveButton_clicked();
+
+    void on_undoButton_clicked();
+
+    void on_clearButton_clicked();
+
 private:
     Ui::Canvas *ui;
     QPainter *paint;
     void paintEvent(QPaintEvent *);
+    QPoint mousePoint;
+    Shape begin_shape;
+    Shape end_shape;
+    int editMethod;
+    int interMethod;
+    int timeMethod;
+    int time;
+    void editPoint();
+    void paintBeginShape();
+    void paintEndShape();
 };
 
 #endif // CANVAS_H
